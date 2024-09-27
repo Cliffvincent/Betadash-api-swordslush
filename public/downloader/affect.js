6eaeadbcfc2519d985bed9659c6c74c17bf8fa10
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-09-28 02:52:32 +0800

    Create affect.js

---
+++ public/downloader/affect.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+exports.name = '/affect';
+exports.index = async (req, res) => {
+  const { uid } = req.query;
+
+  if (!uid) {
+    return res.status(400).json({ error: 'Please provide all required parameters' });
+  }
+
+  try {
+    const response = await axios.get('https://deku-rest-api.gleeze.com/canvas/affect', {
+      responseType: 'arraybuffer',
+      params: { uid }
+    });
+
+    res.set('Content-Type', 'image/png');
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while processing your request' });
+  }
+};
