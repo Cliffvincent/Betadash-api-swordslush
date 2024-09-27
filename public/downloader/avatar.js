da3abf5578a1abb96f232e88164e9a08505eeeec
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-09-28 02:56:15 +0800

    Create avatar.js

---
+++ public/downloader/avatar.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+exports.name = '/avatar';
+exports.index = async (req, res) => {
+  const { id, bgname, signature, color } = req.query;
+
+  if (!id || !bgname || !signature || !color) {
+    return res.status(400).json({ error: 'Please provide all required parameters' });
+  }
+
+  try {
+    const response = await axios.get('https://deku-rest-api.gleeze.com/canvas/avatar', {
+      responseType: 'arraybuffer',
+      params: { id, bgname, signature, color }
+    });
+
+    res.set('Content-Type', 'image/png');
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while processing your request' });
+  }
+};
